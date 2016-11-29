9b0769b1ec78b57061fa54f8d126418072b74103
richtea87 <rich@webworkshop.ltd>
2016-11-29 11:00:21 +0000

    Activated directory indexes

--- config.rb
+++ config.rb
@@ -12,6 +12,8 @@ page '/*.txt', layout: false
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
+activate :directory_indexes
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
